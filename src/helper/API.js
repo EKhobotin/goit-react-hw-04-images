e7779d1f46e7e905e21c969bac6fd264e5ad52e6
EKhobotin <evgen.29.3.85@gmail.com>
2023-12-21 02:51:42 +0200

    images

--- src/helper/API.js
+++ src/helper/API.js
@@ -1,7 +1,7 @@
 export async function getImages(query, page = 1) {
   const BASE_URL = 'https://pixabay.com/api/';
   const API_KEY = '40783621-48111ea33d2baaaa5d03de19e';
-  const PER_PAGE = 3;
+  const PER_PAGE = 12;
   const response = await fetch(
     `${BASE_URL}?key=${API_KEY}&q=${query}&per_page=${PER_PAGE}&page=${page}`
   );
